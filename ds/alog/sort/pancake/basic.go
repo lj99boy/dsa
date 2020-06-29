3abb3f6375c8d44e8addb1034d2f3d43b8f65f1f
lj99boy <lj99boy@hotmail.com>
2020-06-30 01:21:26 +0800

    add basic pancake implement

--- ds/alog/sort/pancake/basic.go
+++ ds/alog/sort/pancake/basic.go
@@ -1,1 +1,22 @@
 package pancake
+
+func Basic(arr []int) []int {
+	for i := len(arr) - 1; i > 0; i-- {
+		maxI := 0
+		for j := i; j > 0; j-- {
+			if arr[j] > arr[maxI] {
+				maxI = j
+			}
+		}
+		flip(arr[0 : maxI+1])
+		flip(arr[0 : i+1])
+	}
+	return arr
+}
+
+//切片内每位反转，如第一位和最后一位，第二位和倒数第二位
+func flip(arr []int) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
+	}
+}
